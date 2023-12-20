91f445ae29fde67947e6d60bb9270b991c8d6e7a
Sara Usevska <sarausevska42@yahoo.com>
2023-12-20 18:07:37 +0100

    first commit

---
+++ src/project41/Person.java
@@ -0,0 +1,15 @@
+package project41;
+
+class Person {
+private String name;
+private int age;
+public Person(String name) {
+this.name=name;
+}
+public void setAge(int age) {
+this.age=age;
+}
+public int getAge() {
+	return age;
+}
+}
